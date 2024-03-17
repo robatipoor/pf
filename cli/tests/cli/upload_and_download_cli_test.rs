00ab45a8f2f678d21aa425237c735ec8d5ff0b4d
Mahdi Robatipoor <mahdi.robatipoor@gmail.com>
2024-03-17 03:54:35 +0330

    add test download_decrypt_command_to_the_destination_file

--- cli/tests/cli/upload_and_download_cli_test.rs
+++ cli/tests/cli/upload_and_download_cli_test.rs
@@ -1,10 +1,11 @@
 use assert_cmd::Command;
+use sdk::util::file::add_extension;
 
 use crate::helper::{generate_random_key_nonce, CliTestContext};
 
 #[test_context::test_context(CliTestContext)]
 #[tokio::test]
-async fn test_upload_and_download_command(ctx: &mut CliTestContext) {
+async fn test_upload_and_download_command_to_the_destination_dir(ctx: &mut CliTestContext) {
   let (file, expected_content) = ctx.create_dummy_file().await.unwrap();
   let url_path = Command::cargo_bin("cli")
     .unwrap()
@@ -46,7 +47,9 @@ async fn test_upload_and_download_command(ctx: &mut CliTestContext) {
 
 #[test_context::test_context(CliTestContext)]
 #[tokio::test]
-async fn test_upload_encrypt_and_download_decrypt_command(ctx: &mut CliTestContext) {
+async fn test_upload_encrypt_and_download_decrypt_command_to_the_destination_dir(
+  ctx: &mut CliTestContext,
+) {
   let key_nonce = generate_random_key_nonce();
   let (file, expected_content) = ctx.create_dummy_file().await.unwrap();
   let url_path = Command::cargo_bin("cli")
@@ -100,7 +103,35 @@ async fn test_upload_encrypt_and_download_decrypt_command(ctx: &mut CliTestConte
 
 #[test_context::test_context(CliTestContext)]
 #[tokio::test]
-async fn test_download_command_with_destination_file(ctx: &mut CliTestContext) {
+async fn test_upload_and_download_command_to_the_destination_file(ctx: &mut CliTestContext) {
+  let (url_path, expected_content) = ctx.upload_dummy_file().await.unwrap();
+  let destination_file_path = ctx.workspace.join("destination_file.text");
+  Command::cargo_bin("cli")
+    .unwrap()
+    .args([
+      "--server-addr",
+      &ctx.server_addr,
+      "download",
+      "--url-path",
+      &url_path.to_string(),
+      "--destination",
+      destination_file_path.to_str().unwrap(),
+      "--progress-bar",
+    ])
+    .assert()
+    .success();
+  let actual_content = tokio::fs::read_to_string(&destination_file_path)
+    .await
+    .unwrap();
+  assert_eq!(actual_content, expected_content);
+}
+
+#[test_context::test_context(CliTestContext)]
+#[tokio::test]
+async fn test_upload_encrypt_and_download_decrypt_command_to_the_destination_file(
+  ctx: &mut CliTestContext,
+) {
+  let key_nonce = generate_random_key_nonce();
   let (file, expected_content) = ctx.create_dummy_file().await.unwrap();
   let url_path = Command::cargo_bin("cli")
     .unwrap()
@@ -110,14 +141,18 @@ async fn test_download_command_with_destination_file(ctx: &mut CliTestContext) {
       "upload",
       "--source-file",
       file.to_str().unwrap(),
+      "--key-nonce",
+      &key_nonce,
       "--output",
       "url-path",
     ])
     .output()
     .unwrap()
     .stdout;
+  let encrypt_file = format!("{}.bin", file.to_str().unwrap());
+  assert!(!tokio::fs::try_exists(encrypt_file).await.unwrap());
   let url_path = std::str::from_utf8(&url_path).unwrap().trim();
-  let destination_file_path = ctx.workspace.join("destination_file.text");
+  let destination_file_path = ctx.workspace.join("destination_file.txt");
   Command::cargo_bin("cli")
     .unwrap()
     .args([
@@ -128,11 +163,14 @@ async fn test_download_command_with_destination_file(ctx: &mut CliTestContext) {
       url_path,
       "--destination",
       destination_file_path.to_str().unwrap(),
-      "--progress-bar",
+      "--key-nonce",
+      &key_nonce,
     ])
     .assert()
     .success();
-  let actual_content = tokio::fs::read_to_string(&destination_file_path)
+  let encrypt_file = add_extension(&destination_file_path, "bin");
+  assert!(!tokio::fs::try_exists(encrypt_file).await.unwrap());
+  let actual_content = tokio::fs::read_to_string(ctx.workspace.join("destination_file"))
     .await
     .unwrap();
   assert_eq!(actual_content, expected_content);
